d7bc09bbd196ed75e499c69b26673432fe174744
Marcelo/ <mfornet94@gmail.com>
2016-11-06 20:55:41 -0800

    Update KMP

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -1,34 +1,36 @@
 /*
-	Tested: SPOJ NHAY
+	Prefix function and Knuth-Morris-Pratt string matching
+
 	Complexity: O(n + m)
+
+	Tested: http://www.spoj.com/problems/NHAY/
 */
 
-vector<int> prefix_function(const string &s)
+vector<int> prefix_function(const string &p)
 {
-	int n = s.length();
-	vector<int> pi(n + 1);
+	int n = p.length();
 
-	for (int i = 0, j = pi[0] = -1; i < n; pi[++i] = ++j)
-		while (j >= 0 && s[i] != s[j]) j = pi[j];
+	vector<int> pref(n + 1);
 
-	return pi;
+	for (int i = 0, j = pref[0] = -1; i < n; pref[++i] = ++j)
+		while (j >= 0 && p[i] != p[j]) j = pref[j];
+
+	return pref;
 }
 
-vector<int> kmp(const string &s, const string &p)
+vector<int> knuth_morris_pratt(const string &s, const string &p)
 {
-	auto pi = prefix_function(p);
 	int n = s.length(), m = p.length();
 
-	vector<int> ans;
+	vector<int> pref = prefix_function(p), matches;
 
 	for (int i = 0, j = 0; i < n; ++i)
 	{
-		while (j >= 0 && s[i] != p[j])
-			j = pi[j];
+		while (j >= 0 && s[i] != p[j]) j = pref[j];
 
 		if (++j == m)
-			ans.push_back(i - m + 1);
+			matches.push_back(i - m + 1), j = pref[j];
 	}
 
-	return ans;
+	return matches;
 }
